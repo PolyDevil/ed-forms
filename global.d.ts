8938b06ddd7cd91abc764125a19bb66f585395f5
Artem Moshkin <wtf@waifu.club>
2025-01-02 12:56:57 +0300

    define Branded type utility

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+// By declaring a unique symbol, we create a distinct marker in TypeScript.
+declare const __brand: unique symbol;
+
+declare global {
+  // Define a Branded type that combines a base type with a brand
+  type Branded<Type, Brand> = Type & {
+    readonly [__brand]: Brand;
+  };
+}
+
+export {};
