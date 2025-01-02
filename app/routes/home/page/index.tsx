45ee49e3009960122f173e741b562d38809b1dcf
Artem Moshkin <wtf@waifu.club>
2025-01-02 13:11:28 +0300

    create simple form

--- app/routes/home/page/index.tsx
+++ app/routes/home/page/index.tsx
@@ -3,15 +3,26 @@ import c from "clsx";
 import s from "./index.module.css";
 
 interface props {
-    className?: string;
+  className?: string;
 }
 
 function Component(props: props) {
   return (
-    <main className={c(s.rootX, props.className)}>
-      hi
-    </main>
+    <form className={c(s.rootX, props.className)}>
+      <input
+        name="code"
+        placeholder="123456 (code)"
+        autoComplete="one-time-code"
+        pattern="\d{6}"
+        inputMode="numeric"
+        minLength={6}
+        maxLength={6}
+        required
+      />
+
+      <button>submit</button>
+    </form>
   );
 }
 
-export default Component
+export default Component;
